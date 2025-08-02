8214d2acbd2245ec47f8b4ff74446ecf6f4222b6
Udoka_AM <udoka.eth@gmail.com>
2025-08-02 23:52:50 +0100

    Update errors.rs

--- programs/nft-staking/src/errors.rs
+++ programs/nft-staking/src/errors.rs
@@ -1,7 +1,7 @@
 use anchor_lang::error_code;
 
 #[error_code]
-pub enum StakeError {
+pub enum StakeErrors {
     #[msg("Frezze period not passed")]
     FreezePeriodNotPassed,
     #[msg("Max stake reached")]
